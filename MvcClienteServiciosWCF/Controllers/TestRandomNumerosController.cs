2f5bbeb39616135f183cccc0df5805ae9bd87d42
serraguti <46522403+serraguti@users.noreply.github.com>
2022-01-14 19:52:50 +0100

    Add project files.

---
+++ MvcClienteServiciosWCF/Controllers/TestRandomNumerosController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcClienteServiciosWCF.Controllers
+{
+    public class TestRandomNumerosController : Controller
+    {
+        ServiceRandomNumbersClient client;
+
+        public TestRandomNumerosController()
+        {
+            this.client =
+                new ServiceRandomNumbersClient
+(ServiceRandomNumbersClient.EndpointConfiguration.BasicHttpBinding_IServiceRandomNumbers);
+        }
+
+        public async Task <IActionResult> Index()
+        {
+            int[] numeros = await this.client.GetRandomNumbersAsync();
+            return View(numeros);
+        }
+    }
+}
